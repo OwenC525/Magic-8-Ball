bcc7e454ac6cada945a13d7a1d9cc49c087846e7
OwenC525 <525ochiu@frhsd.com>
2023-12-15 11:37:59 -0500

    finished tutorial

--- script.js
+++ script.js
@@ -10,4 +10,60 @@ function shakeMagic8Ball(){
         alert('Please enter a valid question!')
         return
     }
+
+    const randomNumber = Math.floor(Math.random()*8);
+    let answer, image, color;
+
+    switch (randomNumber) {
+        case 0:
+            answer = "Yes";
+            image = "yes-image.jpg"; //change this!//
+            color = "text-success";
+            break;
+        case 1:
+            answer = "No";
+            image = "no-image.jpg"; //change this!//
+            color = "text-danger";
+        case 2:
+            answer = "Maybe";
+            image = "maybe-image.jpg"; //change this//
+            color = "text-warning";
+            break;
+        case 3:
+            answer = "Of Course";
+            image = "ofcouse-image.jpg"; //change//
+            color = "text-success";
+            break;
+        case 4:
+            answer = "Definitely";
+            image = "definitely-image.jpg"
+            color = "text-danger";
+            break;
+        case 5:
+            answer = "No";
+            image = "no-image.jpg"; //change this!//
+            color = "text-danger";
+        case 6:
+            answer = "Maybe";
+            image = "maybe-image.jpg"; //change this//
+            color = "text-warning"
+            break;
+        case 7:
+            answer = "Of Course";
+            image = "ofcouse-image.jpg"; //change//
+            color = "text-success";
+            break;
+        case 8:
+            answer = "Definitely";
+            image = "definitely-image.jpg"
+            color = "text-danger";
+            break;
+    }
+
+
+
+    const responseText = document.getElementById('response-text');
+    responseText.innerText = answer;
+    responseText.classList = `display-4 ${color}`;
+    document.getElementById('response-image').src = image;
 }
